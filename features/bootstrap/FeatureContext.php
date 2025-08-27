7255fdcbf000fe5d0e849d3048150b47f2242db4
acoulton <andrew@ingenerator.com>
2025-08-27 11:18:36 +0100

    test: Remove unused `%%TMP_DIR%%` placeholder from features

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -445,7 +445,6 @@ EOL;
     {
         $text = strtr($expectedText, [
             '\'\'\'' => '"""',
-            '%%TMP_DIR%%' => sys_get_temp_dir() . DIRECTORY_SEPARATOR,
             '%%WORKING_DIR%%' => realpath($this->workingDir . DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR,
             '%%DS%%' => DIRECTORY_SEPARATOR,
         ]);
